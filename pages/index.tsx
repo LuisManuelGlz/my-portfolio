59679e8815f2246495c41dd366dc9b701280355f
Luis Manuel González <mannyglzg@gmail.com>
2021-01-06 19:33:43 -0800

    Add showcase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,87 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
 import styles from '../styles/Home.module.css';
 
 const Home = () => {
+  useEffect(() => {
+    const nav = document.querySelector('#nav');
+    const badge = document.querySelector('#badge');
+    const showcase = document.querySelector('#showcase');
+
+    const showcaseOptions = {
+      rootMargin: '-100px 0px 0px 0px',
+    };
+
+    const showcaseObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+          nav.classList.add(styles.scrolled);
+          badge.classList.remove(styles.hide);
+        } else {
+          nav.classList.remove(styles.scrolled);
+          badge.classList.add(styles.hide);
+        }
+      });
+    }, showcaseOptions);
+
+    showcaseObserver.observe(showcase);
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Luis Manuel G.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <header>
+        <nav id="nav" className={styles.nav}>
+          <div>
+            <a id="badge" className={styles.link} href="#">Luis Manuel G.</a>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <a className={styles.link} href="#languages">
+                  Lenguajes y tecnologías
+                </a>
+              </li>
+              <li>
+                <a className={styles.link} href="#projects">
+                  Proyectos personales
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+
+      <main>
+        <section id="showcase" className={styles.showcase}>
+          <video src="./video.mp4" muted loop autoPlay />
+
+          <div className={styles.overlay} />
+
+          <div className={styles.text}>
+            <h1>Luis Manuel G.</h1>
+            <h2>Desarrollador Web Jr</h2>
+          </div>
+        </section>
+
+        <section>
+          <div id="languages" className={styles.languages}>
+            Languages and tools
+          </div>
+        </section>
+
+        <section>
+          <div id="projects" className={styles.projects}>
+            Projects
+          </div>
+        </section>
+      </main>
+
+      {/* <main>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -48,9 +120,9 @@ const Home = () => {
             </p>
           </a>
         </div>
-      </main>
+      </main> */}
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -59,7 +131,7 @@ const Home = () => {
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 };
