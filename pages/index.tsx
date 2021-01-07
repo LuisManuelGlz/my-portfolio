4974e3713ecba63b8909f9870dbf02e88b4f3f48
Luis Manuel González <mannyglzg@gmail.com>
2021-01-07 00:37:31 -0800

    Add smooth behavior

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,6 +79,17 @@ const Home = () => {
         </section>
       </main>
 
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+
       {/* <main>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
