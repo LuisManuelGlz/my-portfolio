5663d3a84f6ffcefedff4c0b7390549b47b8aa8a
Luis Manuel González <luismanuelglz.dev@gmail.com>
2021-08-02 11:30:35 -0700

    Remove Tailwind

--- components/meta/meta.tsx
+++ components/meta/meta.tsx
@@ -4,6 +4,10 @@ const Meta = () => {
   return (
     <Head>
       <link rel="icon" href="/favicon.ico" />
+      {/* <link
+        rel="stylesheet"
+        href="node_modules/modern-normalize/modern-normalize.css"
+      /> */}
     </Head>
   );
 };
