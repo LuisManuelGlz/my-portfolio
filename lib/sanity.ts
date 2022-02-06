4fac9a211064833fe708afc084a67525ab874d28
Luis Manuel González <luismanuelglz.dev@gmail.com>
2022-02-05 19:48:01 -0800

    Update packages to latest

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -5,6 +5,7 @@ const options = {
   // Find your project ID and dataset in `sanity.json` in your studio project
   dataset: 'production',
   projectId: config.projectId,
+  apiVersion: '2021-08-31',
   useCdn: config.useCdn === 'production',
   // useCdn == true gives fast, cheap responses using a globally distributed cache.
   // Set this to false if your application require the freshest possible
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from 'next/app';
+// import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css';
 import { LanguageProvider } from '../contexts/LanguageContext';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <LanguageProvider>
-      <Component {...pageProps} />
-    </LanguageProvider>
+    // <ChakraProvider>
+      <LanguageProvider>
+        <Component {...pageProps} />
+      </LanguageProvider>
+    // </ChakraProvider>
   );
 };
 
